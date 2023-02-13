97112c8221b5ab97d372920aebe2b8864edd7633
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-02-13 10:33:37 -0600

    corrected mistakes

--- js/functions.js
+++ js/functions.js
@@ -9,8 +9,14 @@
  * > sayHello("codeup") // returns "Hello, codeup!"
  */
 
-function sayHello(name) {
-    return (`Hello ${name}`);
+// function sayHello(name) {
+//     let message = (`Hello, ${name}`);
+//     return message;
+// }
+
+const sayHello = (name) => {
+    let message = (`Hello, ${name}`);
+    return message;
 }
 
 //console.log(sayHello('codeup'));
@@ -23,7 +29,7 @@ function sayHello(name) {
  * console.log 'helloMessage' to check your work
  */
 
-let helloMessage = `${sayHello('Connor')}`;
+// let helloMessage = sayHello('Connor');
 
 //console.log(helloMessage);
 
@@ -34,8 +40,10 @@ let helloMessage = `${sayHello('Connor')}`;
  * console.
  */
 
-let myName = 'Connor';
-sayHello(myName);
+
+const myName = 'Connor';
+const helloMessage = sayHello(myName);
+
 
 // Don't modify the following line, it generates a random number between 1 and 3
 // and stores it in a variable named random
@@ -59,7 +67,7 @@ let random = Math.floor((Math.random() * 3) + 1);
  * number)
  */
 
-function isTwo(num) {
+const isTwo = (num) => {
     let number2 = num === 2;
     return number2;
 }
@@ -78,13 +86,21 @@ function isTwo(num) {
  * > calculateTip(0.15, 33.42) // returns 5.013
  */
 
-function calculateTip(total, tip) {
-    let totalTip = (total * (tip / 100));
-    let formattedTip = totalTip.toLocaleString("en-US", {style:"currency", currency:"USD"});
+// const calculateTip = (total, tip) => {
+//     let totalTip = (total * (tip / 100));
+//     let formattedTip = totalTip.toLocaleString("en-US", {style:"currency", currency:"USD"});
+//     return formattedTip;
+// }
+
+const calculateTip = (tipPercentage, billTotal) => {
+    let tipPercentageNum = parseFloat(tipPercentage);
+    let billTotalNum = parseFloat(billTotal);
+    let amountToTip = (tipPercentageNum / 100) * billTotalNum;
+    let formattedTip = amountToTip.toLocaleString("en-US", {style:"currency", currency:"USD"});
     return formattedTip;
 }
 
-//console.log(`The amount you should tip is ${calculateTip(.20, 300)}`);
+//console.log(`The amount you should tip is ${calculateTip(300, 20)}`);
 
 /**
  * TODO:
@@ -94,11 +110,19 @@ function calculateTip(total, tip) {
  */
 
 
-let billTotal = random;
-let restTotal = parseFloat(prompt(`Your bill was ${billTotal}, how much would you like to tip?`));
-let tipTotal = (calculateTip(billTotal, restTotal));
-let formattedCost = tipTotal.toLocaleString("en-US", {style:"currency", currency:"USD"});
-alert(`Your should tip ${formattedCost}`);
+// let billTotal = random;
+// let restTotal = parseFloat(prompt(`Your bill was ${billTotal}, how much would you like to tip?`));
+// let tipTotal = (calculateTip(billTotal, restTotal));
+// let formattedCost = tipTotal.toLocaleString("en-US", {style:"currency", currency:"USD"});
+// alert(`Your should tip ${formattedCost}`);
+
+let userBill = parseFloat(prompt('How much was your bill?'));
+let userTipPercentage = parseFloat(prompt('What percentage would you like to tip?'));
+let tipAmount = calculateTip(userTipPercentage, userBill);
+let tipInDollars = tipAmount.toLocaleString("en-US", {style:"currency", currency:"USD"});
+let tipTotal = alert(tipInDollars);
+
+// console.log(tipTotal);
 
 /**
  * TODO:
@@ -115,13 +139,17 @@ alert(`Your should tip ${formattedCost}`);
  * > applyDiscount(45.99, 0.12) // 40.4712
  */
 
-function applyDiscount(price, discount,) {
-    let originalPrice = price;
-    let discountPercent = discount;
-    let discountTotal = discountPercent * originalPrice;
-    let billTotal = originalPrice - discountTotal;
-    let formattedBill = billTotal.toLocaleString("en-US", {style:"currency", currency:"USD"});
-    return formattedBill;
+// const applyDiscount = (price, discount,) => {
+//     let discountTotal = discountPercent * originalPrice;
+//     let billTotal = originalPrice - discountTotal;
+//     let formattedBill = billTotal.toLocaleString("en-US", {style:"currency", currency:"USD"});
+//     return formattedBill;
+// }
+
+const applyDiscount = (price, discount) => {
+    let appliedDiscount = (parseFloat(price) * parseFloat(discount)) - parseFloat(price);
+    let applyDiscDollars = appliedDiscount.toLocaleString("en-US", {style:"currency", currency:"USD"});
+    return applyDiscDollars;
 }
 
 // console.log(applyDiscount(100, .2));
