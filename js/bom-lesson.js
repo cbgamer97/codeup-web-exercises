7e3592e1b4566edd4e614dae61bedea95db92ce2
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-03-13 12:07:39 -0500

    learning the BOM

---
+++ js/bom-lesson.js
@@ -0,0 +1,22 @@
+let count = 0;
+let max = 10;
+let interval = 1000;
+
+// let intervalId = (function() {
+//     if (count >= max) {
+//         clearInterval(intervalId);
+//         console.log('All done!');
+//     } else {
+//         count++;
+//         console.log('Repeating this line ' + count);
+//     }
+// }, interval);
+
+
+// console.log('5 seconds starts now!');
+// setTimeout(function() {
+//     console.log("It's been 5 seconds!");
+// }, 5000);
+
+
+// window.location.href = 'https://google.com';
