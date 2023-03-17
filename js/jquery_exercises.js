358707bd27e063baad338fc8a1cbb07065dc3f44
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-03-17 11:54:40 -0500

    working with selectors in jquery

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
-    $("html").ready(function (){
-        alert('The page has loaded!');
-    })
+
+    // ALERT ON PAGE LOAD
+    // $("html").ready(function (){
+    //     alert('The page has loaded!');
+    // })
+
+    // ID SELECTOR
+    // $('#samg').click(function(){
+    //     let contents = $('#samg').html();
+    //     alert(contents);
+    // })
+
+    //CLASS SELECTOR
+    // $('.codeup').css('border', '1px solid red');
+
+    // ELEMENT SELECTOR
+
+    $('li').css('font-size', '20px')
+    $('li, h1, p').css('font-family', 'Apple Chancery')
+    $('h1').ready(function (){
+        let contents = $('h1').html();
+       alert(contents);
+    });
 });
