fef85c267dcbc48abf400f75a979c549c20f81f4
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-03-17 12:06:26 -0500

    learning query mouse events

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -16,10 +16,29 @@ $(document).ready(function(){
 
     // ELEMENT SELECTOR
 
-    $('li').css('font-size', '20px')
-    $('li, h1, p').css('font-family', 'Apple Chancery')
-    $('h1').ready(function (){
-        let contents = $('h1').html();
-       alert(contents);
+    // $('li').css('font-size', '20px')
+    // $('li, h1, p').css('font-family', 'Apple Chancery')
+    // $('h1').ready(function (){
+    //     let contents = $('h1').html();
+    //    alert(contents);
+    // });
+
+    // MOUSE EVENTS
+
+    $('h1').click(function (){
+        $(this).css('background-color', 'red');
     });
+
+    $('p').dblclick(function (){
+        $(this).css('font-size', '18px');
+    })
+
+    $('li').hover(
+        function () {
+            $(this).css('color', 'red');
+        },
+        function () {
+            $(this).css('color', 'black');
+        }
+    );
 });
