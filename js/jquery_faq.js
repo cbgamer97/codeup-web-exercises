77526a63397f8953b0d95a8b1f9423183dc8e624
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-03-20 16:30:09 -0500

    finished all toggle functions

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,7 +1,16 @@
-let $toggleClass = $('.invisible');
+
 
 $(document).ready(function() {
     $('#hidden').on('click', function() {
         $('dd').toggleClass('invisible');
+    });
+
+    $('dt').on('click', function (event){
+        event.preventDefault();
+        if ($(this).hasClass('highlight')){
+            $(this).removeClass('highlight');
+        } else {
+            $(this).addClass('highlight');
+        }
     });
 });
