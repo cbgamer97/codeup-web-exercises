1c388d126ff18430699ea08c9ec6fc688b3da543
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-03-19 22:32:15 -0500

    working on konami code function

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
 
     // ALERT ON PAGE LOAD
-    // $("html").ready(function (){
-    //     alert('The page has loaded!');
-    // })
+    $("html").ready(function (){
+        alert('The page has loaded!');
+    })
 
     // ID SELECTOR
-    // $('#samg').click(function(){
-    //     let contents = $('#samg').html();
-    //     alert(contents);
-    // })
+    $('#samg').click(function(){
+        let contents = $('#samg').html();
+        alert(contents);
+    })
 
     //CLASS SELECTOR
-    // $('.codeup').css('border', '1px solid red');
+    $('.codeup').css('border', '1px solid red');
 
     // ELEMENT SELECTOR
 
-    // $('li').css('font-size', '20px')
-    // $('li, h1, p').css('font-family', 'Apple Chancery')
-    // $('h1').ready(function (){
-    //     let contents = $('h1').html();
-    //    alert(contents);
-    // });
+    $('li').css('font-size', '20px')
+    $('li, h1, p').css('font-family', 'Apple Chancery')
+    $('h1').ready(function (){
+        let contents = $('h1').html();
+       alert(contents);
+    });
 
     // MOUSE EVENTS
 
