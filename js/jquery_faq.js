67fadd1be8931aca958043b31b930fe05f5177e6
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-03-21 11:18:34 -0500

    completed bonus problems

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -14,33 +14,54 @@ $(document).ready(function() {
         }
     });
 
-    $('#yellow-li').on('click',function (event) {
+    $('#yellowLi').on('click',function (event) {
         event.preventDefault();
-        $('li').each(function (index){
-            if (index === 3 || index === 7 || index === 11) {
-                $(this).css({
-                    'backgroundColor': 'yellow'
-                })
-            }
-        })
+        $('#parkFacts').find('ul li:last-child').toggleClass('highlight');
     })
 
     $('h3').on('click', function (event){
         event.preventDefault();
-        $('h3').next().css({
-            'fontWeight': 'bold'
-        })
+        let $nextUl = $(this).next();
+        $nextUl.find('li').css('fontWeight', 'bold');
     })
 
     $('li').on('click', function(event){
         event.preventDefault();
-        $('li').each(function(index){
-            if (index === 0 || index === 4 || index === 8){
-                $(this).css({
-                    'color': 'blue'
-                })
-            }
-        })
+        $(this).parent().children().first().css('color', 'blue');
+    })
+
+    $('.swap-btn').on('click', function () {
+        let btn = $(this).attr('id');
+        let $commonParent = $(this).parents('.row');
+        let $images = $commonParent.find('img');
+        let imgSources = [];
+        $images.each(function (){
+            let srcValue = $(this).attr('src');
+           imgSources.push(srcValue);
+        });
+        switch (btn){
+            case 'button1':
+                $($images[0]).attr('src', imgSources[1]);
+                $($images[1]).attr('src', imgSources[0]);
+                console.log('clicked1')
+                break;
+            case 'button2':
+                let randomNum = randomNumber(1, 2);
+                if (randomNum === 1) {
+                    $($images[1]).attr('src', imgSources[0]);
+                    $($images[0]).attr('src', imgSources[1]);
+                } else {
+                    $($images[1]).attr('src', imgSources[2]);
+                    $($images[2]).attr('src', imgSources[1]);
+                }
+                console.log('clicked2')
+                break;
+            case 'button3':
+                $($images[2]).attr('src', imgSources[1]);
+                $($images[1]).attr('src', imgSources[2]);
+                console.log('clicked3')
+                break;
+        }
     })
 });
 
