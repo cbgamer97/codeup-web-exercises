11a34e6045187672ed8d038985cba96dda9f7fcf
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-03-20 22:07:59 -0500

    completed faq excercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -13,4 +13,34 @@ $(document).ready(function() {
             $(this).addClass('highlight');
         }
     });
+
+    $('#yellow-li').on('click',function (event) {
+        event.preventDefault();
+        $('li').each(function (index){
+            if (index === 3 || index === 7 || index === 11) {
+                $(this).css({
+                    'backgroundColor': 'yellow'
+                })
+            }
+        })
+    })
+
+    $('h3').on('click', function (event){
+        event.preventDefault();
+        $('h3').next().css({
+            'fontWeight': 'bold'
+        })
+    })
+
+    $('li').on('click', function(event){
+        event.preventDefault();
+        $('li').each(function(index){
+            if (index === 0 || index === 4 || index === 8){
+                $(this).css({
+                    'color': 'blue'
+                })
+            }
+        })
+    })
 });
+
